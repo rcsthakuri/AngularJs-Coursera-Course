b1af8e2747ffa5a3b77b8d0539ff98c2f089c0f6
Ramchandra <thakuri.ram12@gmail.com>
2022-02-13 03:44:59 +0545

    Implemented assignment requirements.
    - Added `app.js` where AngularJs code is written to implement the assignment requirements.
    -- Bonus optional styling requirements were also implemented using css , `ng-if` directive and tweaking scope property in `app.js. (Assigning empty string to one of message string property before changing another message string property.)
    -- E.g before changing `feedBackMessage`, assign `noDataMessage` to `""`  and vice versa to clean any rendered message on screen.
    -- Integrated `app.js` to `index.html` accordingly to meet assignment requirements.

---
+++ module1-solution/app.js
@@ -0,0 +1,37 @@
+//IIFE
+(function (){
+    "use strict";
+    angular.module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+    
+    LunchCheckController.inject = ["$scope"];
+
+    function LunchCheckController($scope) {
+        $scope.noDataMessage;
+        $scope.feedBackMessage;
+       
+        $scope.check = function () {
+            
+            //Check for empty (empty string or string with just space)
+            if (!$scope.lunchMenu) {
+                $scope.feedBackMessage = "";
+                $scope.noDataMessage = "Please enter data first";
+            }
+            else {
+                //Work Flow - spliting with delmiter ',' -> applying array filter with builtin -
+                // function `Boolean` to remove empty string
+                console.log('here');
+                $scope.noDataMessage = "";
+                var splitLunchMenu = $scope.lunchMenu.split(",").filter(Boolean);
+                if (splitLunchMenu.length <= 3) {
+                    console.log('here again');
+                    $scope.feedBackMessage = "Enjoy!";
+                }
+                else
+                {
+                    $scope.feedBackMessage = "Too much!";
+                }
+            }
+         };
+    }
+})();
